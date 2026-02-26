083395a8b1738f7cd7e2fcd0f741ef636cf2a1a5
Parth Chauhan <universetcenter@gmail.com>
2026-02-26 16:02:19 +0530

    fix: Proxy frontend traffic to Next.js PM2 and remove missing ServeStaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,8 +17,7 @@ import { HotelsModule } from './hotels/hotels.module';
 import { HotelBookingsModule } from './hotel-bookings/hotel-bookings.module';
 import { HealthController } from './health.controller';
 import { ConfigModule } from "@nestjs/config";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "path";
+
 import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
@@ -26,10 +25,7 @@ import { PrismaModule } from "./prisma/prisma.module";
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        ServeStaticModule.forRoot({
-            rootPath: join(__dirname, "..", "legacy_backup", "dist"),
-            exclude: ["/api/(.*)"],
-        }),
+
         EventEmitterModule.forRoot(),
         AuthModule,
         PrismaModule,
